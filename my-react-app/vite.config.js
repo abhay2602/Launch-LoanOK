e126b63878cd5c638a4ea41ef36065720201f771
abhay2602 <97271757+abhay2602@users.noreply.github.com>
2025-03-11 12:39:16 +0530

    Initial Setup Complete

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src', // Alias to simplify imports, optional
+      '@': '/src',
     },
+  },
+  server: {
+    host: true,
+    strictPort: true,
+    cors: true,  // Enable CORS
+    origin: "https://530a-59-96-33-119.ngrok-free.app ", // Allow Ngrok URL
   },
 });
